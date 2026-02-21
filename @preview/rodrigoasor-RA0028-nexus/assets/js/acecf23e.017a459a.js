439d5163445cfe563517fcd84f53a3e86a4c6886
paasapcom <paa@sap.com>
2026-02-21 20:42:56 +0000

    pr build ➝ gh pages

---
+++ @preview/rodrigoasor-RA0028-nexus/assets/js/acecf23e.017a459a.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksap_architecture_center=globalThis.webpackChunksap_architecture_center||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/@preview/rodrigoasor-RA0028-nexus/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/rodrigoasor-RA0028-nexus/blog/authors"}')}}]);
